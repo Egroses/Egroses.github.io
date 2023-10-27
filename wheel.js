89decdaf2f187ac662089df374be562a4f5f1f2c
Emirhan Gül <79425115+Egroses@users.noreply.github.com>
2023-10-27 15:27:12 +0300

    pushagaaa

--- wheel.js
+++ wheel.js
@@ -1,3 +1,5 @@
+console.log("Welcome! 3");
+
 var keycloak = new Keycloak({
     url: "https://sso.consumer.games/auth",
     realm: "magic",
@@ -16,7 +18,10 @@ async function initializeKeycloak() {
             `User is ${authenticated ? "authenticated" : "not authenticated"}`
         );
         console.log(keycloak.token, " wow");
+        document.getElementById("login-button").style("display", "none");
+        document.getElementById("unity-container").style("display", "block");
     } catch (error) {
+        console.error("Failed to initialize adapter:", error.toString());
         console.error("Failed to initialize adapter:", JSON.stringify(error));
     }
 }
